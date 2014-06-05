86eb55f6ff2b6e71a6d6cf01e524c3fac8b3ac0f
Peter Mottram <peter@sysnix.com>
2014-06-05 11:34:32 +0200

    remove perdancer*.jpg, add manifest & pod tests + update MANIFEST

--- DanceShop/lib/DanceShop.pm
+++ DanceShop/lib/DanceShop.pm
@@ -1,12 +1,25 @@
+use utf8;
 package DanceShop;
+
+=encoding utf8
+
+=head1 NAME
+
+DanceShop - base Demo Shop for Interchange 6
+
+=head1 VERSION
+
+0.001
+
+=cut
+
+our $VERSION = '0.001';
 
 use Dancer ':syntax';
 use Dancer::Plugin::Auth::Extensible;
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Interchange6;
 use Dancer::Plugin::Interchange6::Routes;
-
-our $VERSION = '0.0001';
 
 set session => 'DBIC';
 set session_options => {schema => schema};
