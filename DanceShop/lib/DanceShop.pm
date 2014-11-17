4a9a575b332c750808423a052184b62738f17cc1
Peter Mottram <peter@sysnix.com>
2014-11-17 15:33:59 +0100

    Allow selection of navigation view so only one is visible
    switch of navigation view is currently via query arg and should be
    changed to something stored in session

--- DanceShop/lib/DanceShop.pm
+++ DanceShop/lib/DanceShop.pm
@@ -20,6 +20,7 @@ use Dancer::Plugin::Auth::Extensible;
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Interchange6;
 use Dancer::Plugin::Interchange6::Routes;
+use DateTime;
 
 use DanceShop::Routes::Checkout;
 
@@ -43,6 +44,61 @@ hook 'before_layout_render' => sub {
     while (my $record = $nav->next) {
         push @{$tokens->{'nav-' . $record->scope}}, $record;
     };
+};
+
+hook 'before_navigation_display' => sub {
+    my $tokens = shift;
+    
+    my %querystring_params = params('query');
+
+    # FIXME: view should be stored in session so that it is preserved across
+    # requests as user navigates around shop. For now this is at least useful
+    # for testing
+    my $route_config = config->{plugin}->{'Interchange6::Routes'};
+    my $view = $querystring_params{view};
+    if ( defined $view ) {
+        undef $view unless grep { $_ eq $view } (qw/grid list simple compact/);
+    }
+    unless ( defined $view ) {
+        $view = $route_config->{navigation}->{default_view} || 'list';
+    }
+    $tokens->{"navigation-view-$view"} = 1;
+
+    # FIXME: following still needs thought & fixing...
+    # we need to add selling_price to the products token
+    # TODO: this should be handled in schema/plugin especially since
+    # the plugin might pass a resultset or an array of products depending
+    # on the template engine in use
+
+#    my $roles;
+#    if ( logged_in_user ) {
+#        $roles = user_roles;
+#        push @$roles, 'authenticated';
+#    }
+#    push @$roles, 'anonymous';
+
+#    my $dtf = shop_schema->storage->datetime_parser;
+#    my $today = $dtf->format_datetime(DateTime->today);
+#    my $rset = $tokens->{products}->search(
+#        {
+#            'role.name'                => { -in => $roles },
+#            'price_modifiers.quantity' => 1,
+#            'price_modifiers.start_date' => [ undef, { '<=', $today } ],
+#            'price_modifiers.end_date'   => [ undef, { '>=', $today } ],
+#        },
+#        {
+#            join => { 'price_modifiers' => 'role' },
+#            '+select' => [ { min => 'price_modifiers.price' } ],
+#            '+as' => [ 'selling_price' ],
+#            group_by => [ 'product.sku' ],
+#        }
+#    );
+#    $rset = $tokens->{products}->as_subselect_rs->search(
+#        {},
+#    );
+    #$rset = $tokens->{products}->with_selling_price($roles);
+    #$tokens->{products} = $rset;
+    #info $rset->count;
 };
 
 hook 'before_product_display' => sub {
