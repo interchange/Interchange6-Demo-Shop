2ea1cb3e91fb4fe832e5ae55f6eb667888a620c3
Peter Mottram <peter@sysnix.com>
2015-01-06 18:12:03 +0100

    add RatingClass filter class

---
+++ DanceShop/lib/DanceShop/Filters/RatingClass.pm
@@ -0,0 +1,30 @@
+package DanceShop::Filters::RatingClass;
+
+=head1 NAME
+
+DanceShop::Filters::RatingClass - product rating to class name converter
+
+=head1 DESCRIPTION
+
+Converts decimal product ratings into css class names required to display the
+correct rating stars sprite.
+
+Examples:
+
+  1.2 => rating-10
+  0.3 => rating-05
+  4.8 => rating-50
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'Template::Flute::Filter';
+
+sub filter {
+    my ($self, $value) = @_;
+    return "rating-" . sprintf( "%02d", sprintf( "%.0f", $value * 2 ) * 5 );
+}
+
+1;
