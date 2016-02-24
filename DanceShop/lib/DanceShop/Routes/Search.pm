bfc221a284a812e32d2607974d02566a91097824
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-02-24 11:33:06 +0100

    Add prices to Solr search display.

--- DanceShop/lib/DanceShop/Routes/Search.pm
+++ DanceShop/lib/DanceShop/Routes/Search.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use DanceShop::Search;
 use Dancer ':syntax';
+use Dancer::Plugin::Interchange6;
 
 use Interchange::Search::Solr;
 
@@ -12,7 +13,7 @@ get '/search' => sub {
     my $q = param('q');
     my $tokens = {};
 
-    my $sr = Interchange::Search::Solr->new(
+    my $solr = Interchange::Search::Solr->new(
         solr_url => config->{solr_url},
         global_conditions => {
             active => \"true",
@@ -20,9 +21,23 @@ get '/search' => sub {
         facets => [],
     );
 
-    $sr->search($q);
+    $solr->search($q);
 
-    $tokens->{products} = $sr->results;
+    my @products;
+    my $rs = shop_schema->resultset('Product');
+
+    for my $res (@{$solr->results}) {
+        my $product = $rs->find($res->{sku});
+        next unless $product;
+
+        $res->{selling_price} = $product->selling_price;
+        $res->{variant_count} = $product->variant_count;
+        $res->{discount} = $product->discount_percent;
+
+        push @products, $res;
+    }
+
+    $tokens->{products} = \@products;
 
     template 'product-listing-list', $tokens;
 };
