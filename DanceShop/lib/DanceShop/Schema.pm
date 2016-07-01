e5b50f236520d9d70e65307344b95e042566b6f0
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-07-01 09:36:14 +0200

    Fix bogus result_namespace and resultset_namespace in DanceShop::Schema.

--- DanceShop/lib/DanceShop/Schema.pm
+++ DanceShop/lib/DanceShop/Schema.pm
@@ -8,8 +8,8 @@ __PACKAGE__->load_components( 'Schema::Config' );
 
 Interchange6::Schema->load_namespaces(
     default_resultset_class => 'ResultSet',
-    result_namespace        => [ 'Result', '+PerlDance::Schema::Result' ],
-    resultset_namespace     => [ 'ResultSet', '+PerlDance::Schema::ResultSet' ],
+    result_namespace        => [ 'Result', '+DanceShop::Schema::Result' ],
+    resultset_namespace     => [ 'ResultSet', '+DanceShop::Schema::ResultSet' ],
 );
 
 1;
